9271fda458c608aacc9578228150e2dd1cdaa5c2
bsdfzzzy <1002380934@qq.com>
2016-05-26 17:01:54 +0800

    add django

--- IOS/wsgi.py
+++ IOS/wsgi.py
@@ -19,7 +19,7 @@ import os
 import sys
 
 root = os.path.dirname(__file__)
-sys.path.insert(0, os.path.join(root, 'site-packages'))
+sys.path.insert(0, os.path.join(root, '..', 'site-packages'))
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "honey.settings")
 
 from django.core.wsgi import get_wsgi_application
