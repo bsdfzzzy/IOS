622578e7eb8c656523ac3ca834e59a19eebcc745
bsdfzzzy <1002380934@qq.com>
2016-05-26 16:38:12 +0800

    change wsgi

--- IOS/wsgi.py
+++ IOS/wsgi.py
@@ -7,10 +7,20 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
 
-import os
+'''import os
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "IOS.settings")
 
+application = get_wsgi_application()'''
+
+import os
+import sys
+
+root = os.path.dirname(__file__)
+sys.path.insert(0, os.path.join(root, '..', 'site-packages'))
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "honey.settings")
+
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
