1f05a4d605b9f5adfb2da3406d7f80835371a598
bsdfzzzy <1002380934@qq.com>
2016-05-26 17:20:20 +0800

    1

--- IOS/wsgi.py
+++ IOS/wsgi.py
@@ -18,10 +18,9 @@ application = get_wsgi_application()'''
 
 import os  
 import sys  
-root = os.path.dirname(__file__)                                             #输出当前文件所在文件夹的绝对路径  
-sys.path.insert(0, os.path.join(root, '..', 'site-packages'))                #将root路径中的上级目录的site-packages加入到Path中去  
+root = os.path.dirname(__file__)                                             
+sys.path.insert(0, os.path.join(root, '..', 'site-packages'))     
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pythondjangotest.settings")  
-  
-  
+
 from django.core.wsgi import get_wsgi_application  
 application = get_wsgi_application() 
